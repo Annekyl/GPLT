6b936f23a0ae3a6a2a4393c6ea9bdc060ca38902
Annekyl <306942709@qq.com>
2026-03-20 13:48:35 +0800

    3-19

---
+++ L2-014 列车调度.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+#define int long long
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int n;
+
+void solve() { 
+    cin >> n;
+    vi a(n);
+    
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,14 +1,31 @@
 #include "bits/stdc++.h"
+#define int long long
 
 using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
 
 int n;
+vi tail;
 
-void solve() { 
-    cin >> n; 
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        int l = 0, r = tail.size();
+        auto it = lower_bound(tail.begin(), tail.end(), x);
+        if (it == tail.end()) {
+            tail.push_back(x);
+        } else {
+            *it = x;
+        }
+    }
+    cout << tail.size() << endl;
 }
 
-int main() {
+signed main() {
     int t = 1;
     // cin >> t;
     while (t--) {
