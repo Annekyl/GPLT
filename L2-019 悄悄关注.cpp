80dc0ba877b99c4400392a0ffcd3062f3f4a09b0
unknown <306942709@qq.com>
2026-03-21 12:55:57 +0800

    3-21

---
+++ L2-019 悄悄关注.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+#define int long long
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int n;
+
+void solve() {
+    cin >> n;
+
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
