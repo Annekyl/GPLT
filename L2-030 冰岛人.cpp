9d4b4aa1415a292e7c85ef4a7e7569cdb3b15798
Annekyl <306942709@qq.com>
2026-03-25 11:52:05 +0800

    3-22

---
+++ L2-030 冰岛人.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+#define int long long
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int N = 1e5 + 10;
+
+int n;
+
+void solve() {
+    cin >> n;
+
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
